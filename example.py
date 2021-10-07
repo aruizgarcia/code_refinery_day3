f8cd52a62cd0e4b7733dc8c73e493a0734fb226d
aruizgarcia <alberto.rzg@gmail.com>
2021-10-07 12:15:51 +0200

    Updated example.py
    Updated example.py for pytest

--- example.py
+++ example.py
@@ -1,5 +1,13 @@
+import pytest
 
-temperature = 10
+def add(a, b):
+    return a + b
 
-def print_temperature():
-    print(temperature)
+def test_add():
+    assert add(2,3) == 5
+    assert add('space', 'ship') == 'spaceship'
+
+def test_add_float():
+    epsilon = 1e-10
+    assert abs(add(0.1, 0.2) - 0.3) < epsilon
+    assert add(0.1, 0.2) == pytest.approx(0.3)
